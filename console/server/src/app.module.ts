6010785dbcb47a56d0d4ac85e658e7e659626b00
SteadEXE <j0rd4n68@gmail.com>
2021-06-15 17:47:40 +0200

    dev: bootstrap vue.

--- console/server/src/app.module.ts
+++ console/server/src/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { configuration } from './configuration/configuration';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-	imports: [],
-	controllers: [ AppController ],
-	providers: [ AppService ],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: [
+        '.env.dev.local',
+        '.env.dev',
+        '.env.production.local',
+        '.env.production',
+        '.env.local',
+        '.env'
+      ],
+      load: [configuration]
+    }),
+    MongooseModule.forRoot(process.env.MONGODB),
+    AuthModule,
+    UserModule
+  ],
+  controllers: [AppController],
+  providers: [AppService]
 })
-export class AppModule {
-}
+export class AppModule {}
